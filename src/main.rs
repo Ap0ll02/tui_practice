d5ed19b7958c203357b3f72c84e73f0cade1da63
Jack <jratermann@unr.edu>
2025-05-05 23:04:43 -0700

    this is a lot of progress. multi-state display.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use std::{io};
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use chrono::TimeDelta;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, MouseEventKind};
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Direction, Layout, Rect},
     style::{Style, Styled, Stylize},
     symbols::border,
     text::{Line, Text},
-    widgets::{Block, Borders, Paragraph, Widget},
+    widgets::{Block, Borders, Padding, Paragraph, Widget},
     DefaultTerminal, Frame,
 };
+
 
 // Create an App struct first
 #[derive(Debug, Default)]
 pub struct App {
-    counter: u8,
     exit: bool,
-    render_count: u32,
+    state: State,
+    my_path: String,
+}
+
+#[derive(Default, Debug)]
+pub enum State {
+    #[default] Intro,
+    Main,
+    Exit,
 }
 
 fn main() -> io::Result<()> {
@@ -28,7 +37,6 @@ fn main() -> io::Result<()> {
 impl App {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
-            self.render_count += 1;
             terminal.draw(|frame| self.draw(frame))?;
             self.handle_events()?;
         }
@@ -44,6 +52,10 @@ impl App {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
             }
+            Event::Mouse(mouse_event) if mouse_event.kind == MouseEventKind::Moved => {
+                let my_pa = self.my_update();
+                self.update_path(my_pa);
+            }
             _ => {}
         };
         Ok(())
@@ -52,76 +64,85 @@ impl App {
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit = true,
-            KeyCode::Left | KeyCode::Char('+') => self.increment_counter(),
-            KeyCode::Right | KeyCode::Char('-') => self.decrement_counter(),
+            KeyCode::Char('n') => self.state = State::Main,
             _ => {}
         }
     }
 
-    fn decrement_counter(&mut self) {
-        if self.counter == 0 {
-            self.counter = 0;
-        } else {
-            self.counter -= 1;
-        }
+    fn update_path(&mut self, path: String) {
+        self.my_path = path;
     }
 
-    fn increment_counter(&mut self) {
-            self.counter += 1;
+    fn my_update(&self) -> String {
+        let mut path: String = "OOOOOO".to_string();
+        let speed = 1;
+        for _ in 0..speed {
+            path += " ";
+        }
+        path += "O";
+        path
     }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Counter App Tutorial ".bold());
+        let title = Line::from(" TOWER DEFENSE ULTIMATE ".blue().bold()).centered();
         let chunks = Layout::default()
             .direction(Direction::Vertical)
             .constraints([
-                Constraint::Min(2),
-                Constraint::Length(8),
-                Constraint::Min(2),
+                Constraint::Percentage(9),
+                Constraint::Percentage(70),
+                Constraint::Percentage(21),
             ])
-            .margin(3)
-            .spacing(5)
+            .vertical_margin(1)
             .split(area);
-        let instructions = Line::from(vec![
-            " + ".blue().into(),
-            " - ".red().into(),
-            " q ".yellow().into(),
-        ]);
-
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-
-        let counter_text = Text::from(vec![Line::from(vec![
-                "Value: ".green().into(),
-                self.counter.to_string().light_blue(),
-        ])]);
-
-        let render_text = 
-            Line::from(vec![
-                " Render Counter ".blue().into(),
-            ]); 
-
-
-        let text_time = Line::from(self.render_count.to_string().cyan());
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(chunks[0] , buf);
+        // ==============================
+        // = BLOCK MANAGEMENT & PREFABS =
+        // ==============================
+        
+        // Easy Border With Thick Rounded Green Borders
+        let easy_block = Block::default()
+            .borders(Borders::ALL)
+            .style(Style::new().green().bold())
+            .border_type(ratatui::widgets::BorderType::Rounded);
 
         let b = Block::default()
-            .title(render_text)
-            .padding(ratatui::widgets::Padding::vertical(2))
+            .title(title)
+            .padding(Padding::vertical(2))
             .style(Style::new().cyan().bold())
             .border_type(ratatui::widgets::BorderType::Rounded)
             .borders(Borders::ALL);
-        Paragraph::new(text_time)
+
+        // =============
+        // = RENDERING =
+        // =============
+        let begin = Paragraph::new("There Ya Go")
             .centered()
-            .block(b)
-            .render(chunks[1], buf);
+            .white()
+            .block(easy_block.clone());
+
+        let begin2 = Paragraph::new("I've Designed This Game To Teach You VIM Motions")
+            .centered()
+            .light_magenta()
+            .block(b);
+
+        let main_move = Paragraph::new(self.my_path.clone())
+            .centered()
+            .light_cyan()
+            .block(easy_block.clone());
+
+        // State Rendering
+        match self.state {
+            State::Intro => {
+                begin.render(chunks[0], buf);
+                begin2.render(chunks[1], buf);
+            }
+            State::Main => {
+                main_move.render(chunks[1], buf);
+            }
+            _ => {}
+        }
     }
 }
+
 
